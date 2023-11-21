4a4954c8c28313881fef66c77ed585dbd689dc3a
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2023-11-21 11:07:37 +0900

    Remove: console & parallax

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,94 +1,107 @@
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
-import ContentContainer from "../components/ContentContainer";
-import IntroBlock from "../components/IntroBlock";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Main = () => {
-  // const tRef = useRef<HTMLDivElement | null>(null);
   const pRef = useRef<HTMLParagraphElement | null>(null);
 
   useEffect(() => {
-    // const targets = gsap.utils.toArray(document.querySelectorAll(".target"));
-    // targets.forEach(t => {
-      // let txt = t.innerText;
-    // })
-    const targets = document.querySelectorAll(".target") as NodeListOf<HTMLParagraphElement>;
-    targets.forEach(t => {
-      let txt = t.innerText;
-      let newTxt = "";
+    const animate = (item: HTMLElement) => {
+      let x = 100;
+      let y = 0;
 
-      for(let i = 0; i < txt.length; i++) {
-        newTxt += "<span aria-hidden='true'>";
-        txt[i] === " " ? newTxt += " " : newTxt += txt[i];
-        newTxt += "</span>";
-      }
-      t.innerHTML = newTxt;
-      t.setAttribute("aria-hidden", txt);
-    }); 
+      item.classList.contains("lTr") ? (x = -100, y = 0) : item.classList.contains("bTt") ? (x = 0, y = 100) : null;
 
-    gsap.utils.toArray<HTMLElement>(".target").forEach(txt => {
-      gsap.from(txt.querySelectorAll("span"), {
-        yPercent: 100,
-        autoAlpha: 0,
-        duration: 0.5,
-        ease: "circ.out",
-        //stagger: 0.04,
-        stagger: {
-          amount: 1,
-          from: "random"
-        },
-        scrollTrigger: {
-          trigger: txt,
-          start: "top bottom",
-          end: "+=400",
-          // markers: true,
-        }
-      })
-    })
+      gsap.fromTo(
+        item, 
+        { autoAlpha: 0, x, y }, 
+        { autoAlpha: 1, x: 0, y: 0, delay: .2, duration: 1.25, overwrite: "auto", ease: "expo" }
+      );
+    }
+
+    const iter = gsap.utils.toArray<HTMLElement>(".target")[Symbol.iterator]()
+    for(const i of iter) {
+      ScrollTrigger.create({
+        trigger: i,
+        start: "top 80%",
+        end: "bottom 20%",
+        onEnter: () => {animate(i)}
+      });
+      i.style.opacity = "0";
+    }
   }, []);
 
   return (
     <Container>
-      <IntroBlock>
-        <IntroItem ref={pRef} className="target">
+      <ParalSection>
+        <TargetP ref={pRef} className="target">
           JavaScript를 사랑하는 2년차 개발자 입니다.
-        </IntroItem>
-      </IntroBlock>
-      <IntroBlock $rTl >
-        <IntroItem ref={pRef} className="target">
+        </TargetP>
+      </ParalSection>
+      <ParalSection>
+        <TargetP ref={pRef} className="target lTr">
           배우는 것에 두려움이 없습니다.
-        </IntroItem>
-      </IntroBlock>
-      <IntroBlock>
-        <IntroItem ref={pRef} className="target">
+        </TargetP>
+      </ParalSection>
+      <ParalSection>
+        <TargetP ref={pRef} className="target">
           JavaScript 기반 Front-end에 기술에 관심이 있습니다.
-        </IntroItem>
-      </IntroBlock>
-      <IntroBlock $rTl>
-        <IntroItem ref={pRef} className="target">
+        </TargetP>
+      </ParalSection>
+      <ParalSection>
+        <TargetP ref={pRef} className="target lTr">
           Back-end에도 소홀하지 않습니다.
-        </IntroItem>
-      </IntroBlock>
-      <IntroBlock>
-        <IntroItem ref={pRef} className="target">
+        </TargetP>
+      </ParalSection>
+      <ParalSection>
+        <TargetP ref={pRef} className="target bTt">
           JavaScript는 사랑입니다...
-        </IntroItem>
-      </IntroBlock>
+        </TargetP>
+      </ParalSection>
     </Container>
   )
 }
 
 export default Main;
 
-const Container = styled(ContentContainer)`
-  overflow: hidden;
-`
+const Container = styled.main`
+  max-width: 1600px;
+  width: 98%;
+  margin: 0 auto;
+  /* overflow: hidden; */
+`;
 
-const IntroItem = styled.p`
-  span {
-    display: inline-block;
+const ParalSection = styled.section`
+  width: 1200px;
+  max-width: 70vw;
+  margin: 30vw auto;
+
+  text-align: left;
+  margin-right: 0;
+  position: relative;
+  padding-top: 15vw;
+
+  &:nth-child(even) {
+    /* margin-right: auto; */
+    margin-left: 0;
+    text-align: right;
   }
+
+  &:nth-child(even) p {
+    margin-left: auto;
+    margin-left: 5vw;
+    text-align: left;
+  }
+`;
+
+const TargetP = styled.p`
+  font-size: 4vw;
+  line-height: 1.4;
+  margin-top: -5vw;
+  margin-left: -4vw;
+  z-index: 100;
+  position: relative;
+  word-break: keep-all;
 `;
