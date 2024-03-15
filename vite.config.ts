963b935b6ae31b27b1e94aa24d7170f3462f9fb5
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-03-15 11:01:56 +0900

    Update: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "./build",
   },
 });
