4b7601036bb004daa11ca6cb490b6c03bb2df86e
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-03-28 11:20:08 +0900

    Update: seo

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import GlobalStyles from './lib/styles/GlobalStyles.ts';
-import { HelmetProvider } from 'react-helmet-async';
-import { ThemeProvider } from 'styled-components';
-import { themes } from './lib/styles/themes.ts';
-import rootReducer from './stroe/index.ts';
-import { legacy_createStore as createStore, applyMiddleware, Middleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import GlobalStyles from "./lib/styles/GlobalStyles.ts";
+import { HelmetProvider } from "react-helmet-async";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./lib/styles/themes.ts";
+import rootReducer from "./stroe/index.ts";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  Middleware,
+} from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { composeWithDevTools } from "redux-devtools-extension"
+import { composeWithDevTools } from "redux-devtools-extension";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { hydrate } from "react-dom";
 // import './index.css';
 
 const l: Middleware = (store) => (next) => (action) => {
@@ -22,13 +27,50 @@ const l: Middleware = (store) => (next) => (action) => {
   const result = next(action);
   // console.log(action, store.getState(), "<< next store");
   return result;
-}
+};
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, l)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, l))
+);
 const client = new QueryClient();
-const root = document.getElementById('root') as HTMLElement;
+const root = document.getElementById("root") as HTMLElement;
+const app = (
+  <QueryClientProvider client={client}>
+    <ReactQueryDevtools />
+    <Provider store={store}>
+      <BrowserRouter>
+        <HelmetProvider>
+          <GlobalStyles />
+          <ThemeProvider theme={{ themes }}>
+            <App />
+          </ThemeProvider>
+        </HelmetProvider>
+      </BrowserRouter>
+    </Provider>
+  </QueryClientProvider>
+);
+
+root?.hasChildNodes() ? hydrate(app, root) : render(app, root);
+
+// ReactDOM.createRoot(root).render(
+//   <HelmetProvider>
+//     <QueryClientProvider client={client}>
+//       <ReactQueryDevtools />
+//       <Provider store={store}>
+//         <BrowserRouter>
+//           <GlobalStyles />
+//           <ThemeProvider theme={{ themes }}>
+//             <App />
+//           </ThemeProvider>
+//         </BrowserRouter>
+//       </Provider>
+//     </QueryClientProvider>
+//   </HelmetProvider>
+// );
+
 // root.hasChildNodes() ? (
-//   ReactDOM.hydrateRoot(root, 
+//   ReactDOM.hydrateRoot(root,
 //     <HelmetProvider>
 //       <QueryClientProvider client={client}>
 //         <ReactQueryDevtools/>
@@ -36,7 +78,7 @@ const root = document.getElementById('root') as HTMLElement;
 //           <BrowserRouter>
 //             <GlobalStyles />
 //             <ThemeProvider theme={{ themes }}>
-//               <App /> 
+//               <App />
 //             </ThemeProvider>
 //           </BrowserRouter>
 //         </Provider>
@@ -53,7 +95,7 @@ const root = document.getElementById('root') as HTMLElement;
 //           <BrowserRouter>
 //             <GlobalStyles />
 //             <ThemeProvider theme={{ themes }}>
-//               <App /> 
+//               <App />
 //             </ThemeProvider>
 //           </BrowserRouter>
 //         </Provider>
@@ -61,20 +103,3 @@ const root = document.getElementById('root') as HTMLElement;
 //     </HelmetProvider>
 //   )
 // )
-
-ReactDOM.createRoot(root).render(
-  <HelmetProvider>
-    <QueryClientProvider client={client}>
-      <ReactQueryDevtools/>
-      <Provider store={store}>
-        <BrowserRouter>
-          <GlobalStyles />
-          <ThemeProvider theme={{ themes }}>
-            <App /> 
-          </ThemeProvider>
-        </BrowserRouter>
-      </Provider>
-    </QueryClientProvider>
-  </HelmetProvider>
-);
-
